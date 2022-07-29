1502bed06ddbade964fdd66db428cba8ebf1a6b1
Redvanskyi-Rost <104914558+Redvanskyi-Rost@users.noreply.github.com>
2022-07-29 12:13:10 +0000

    Deploying to gh-pages from @ Redvanskyi-Rost/IceCream@d0769016288268c768d18359c1ee8f84bf28c05e 🚀

---
+++ index.15d00278.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){var e=window.pageYOffset||document.documentElement.scrollTop;document.querySelector(".header-scroll").style.background=0!==e?"rgba(255, 184, 202, 0.7)":"transparent"};
+//# sourceMappingURL=index.15d00278.js.map
---
+++ index.2800e84c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0})}));
+//# sourceMappingURL=index.2800e84c.js.map
